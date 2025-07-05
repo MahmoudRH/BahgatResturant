de02ca8c75ada4b7c56d4c4b3037608c2e076b01
Mahmoud Habib <mhabeeb5@students.iugaza.edu.ps>
2025-07-05 18:31:25 +0300

    refactored welcome screens

--- app/src/main/java/com/mahmoudrh/bahgatresturant/ui/weclome/SplashScreen.kt
+++ app/src/main/java/com/mahmoudrh/bahgatresturant/ui/weclome/SplashScreen.kt
@@ -3,15 +3,16 @@ package com.mahmoudrh.bahgatresturant.ui.weclome
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.mahmoudrh.bahgatresturant.R
 import com.mahmoudrh.bahgatresturant.ui.ui_components.Logo
-import com.mahmoudrh.bahgatresturant.ui.ui.theme.BahgatResturantTheme
 import kotlinx.coroutines.delay
 
 @Composable
@@ -20,15 +21,19 @@ fun SplashScreen(navigateToPageView: () -> Unit) {
         delay(500)
         navigateToPageView()
     }
-    BahgatResturantTheme {
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.ic_splash_background),
-                contentDescription = null,
-                Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-            Logo(modifier = Modifier.align(Alignment.Center))
-        }
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.ic_splash_background),
+            contentDescription = null,
+            Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Logo(modifier = Modifier.align(Alignment.Center))
     }
+}
+
+@Preview
+@Composable
+private fun SplashPrev() {
+    Surface() { SplashScreen { } }
 }
--- app/src/main/java/com/mahmoudrh/bahgatresturant/ui/weclome/WelcomeScreen.kt
+++ app/src/main/java/com/mahmoudrh/bahgatresturant/ui/weclome/WelcomeScreen.kt
@@ -4,67 +4,68 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mahmoudrh.bahgatresturant.R
-import com.mahmoudrh.bahgatresturant.ui.ui_components.buttons.OutlinedButton
-import com.mahmoudrh.bahgatresturant.ui.ui_components.buttons.FilledButton
-import com.mahmoudrh.bahgatresturant.ui.ui_components.Logo
-import com.mahmoudrh.bahgatresturant.ui.ui.theme.BahgatResturantTheme
 import com.mahmoudrh.bahgatresturant.ui.ui.theme.metropolisFontFamily
 import com.mahmoudrh.bahgatresturant.ui.ui.theme.secondaryFontColor
+import com.mahmoudrh.bahgatresturant.ui.ui_components.Logo
+import com.mahmoudrh.bahgatresturant.ui.ui_components.buttons.FilledButton
+import com.mahmoudrh.bahgatresturant.ui.ui_components.buttons.OutlinedButton
 
 @Composable
-fun WelcomeScreen(navigateToSignIn:()-> Unit , navigateToSignUp:()-> Unit) {
-    BahgatResturantTheme {
-        Column(
+fun WelcomeScreen(navigateToSignIn: () -> Unit, navigateToSignUp: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopSection()
+        Spacer(modifier = Modifier.size(46.dp))
+        Text(
+            text = stringResource(R.string.welcome_title),
+            style = TextStyle(
+                color = secondaryFontColor,
+                fontFamily = metropolisFontFamily,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier.padding(horizontal = 40.dp)
+        )
+        FilledButton(
             modifier = Modifier
-                .fillMaxWidth()
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(horizontal = 34.dp)
+                .padding(top = 56.dp),
+            text = stringResource(R.string.login)
         ) {
-            TopSection()
-            Spacer(modifier = Modifier.size(46.dp))
-            Text(
-                text = "Discover the best foods from over 1,000 restaurants and fast delivery to your doorstep",
-                style = TextStyle(
-                    color = secondaryFontColor,
-                    fontFamily = metropolisFontFamily,
-                    fontSize = 13.sp,
-                    fontWeight = FontWeight.Medium,
-                    textAlign = TextAlign.Center
-                ),
-                modifier = Modifier.padding(horizontal = 40.dp)
-            )
-            FilledButton(
-                modifier = Modifier
-                    .padding(horizontal = 34.dp)
-                    .padding(top = 56.dp),
-                text = "Login"
-            ) {
-                navigateToSignIn()
-            }
-            OutlinedButton(
-                modifier = Modifier
-                    .padding(horizontal = 34.dp, vertical = 24.dp),
-                text = "Create an Account",
-            ) {
-                navigateToSignUp()
-            }
+            navigateToSignIn()
+        }
+        OutlinedButton(
+            modifier = Modifier
+                .padding(horizontal = 34.dp, vertical = 24.dp),
+            text = stringResource(R.string.create_an_account),
+        ) {
+            navigateToSignUp()
         }
     }
 }
@@ -82,5 +83,15 @@ private fun TopSection() {
         )
         Logo(modifier = Modifier.align(Alignment.BottomCenter))
     }
+}
 
+@Preview
+@Composable
+private fun WelcomePrev() {
+    Surface {
+        WelcomeScreen(
+            navigateToSignIn = {},
+            navigateToSignUp = {}
+        )
+    }
 }
